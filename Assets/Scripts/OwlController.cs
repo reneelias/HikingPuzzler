9f72b8e3e8bec4d93b8ec1c69bd01d903018e01c
Rene Elias <reneelias03@gmail.com>
2024-10-14 10:44:56 -0700

    Take off duration can now be controlled completely

--- Assets/Scripts/OwlController.cs
+++ Assets/Scripts/OwlController.cs
@@ -18,15 +18,19 @@ public class OwlController : MonoBehaviour
     [Header("TakeOff")]
     [SerializeField] private Vector3 takeOffOffset = new Vector3(0f, 30f, 0f);
     [SerializeField] private Vector3 takeOffTargetRotation = new Vector3(0f, 360f, 0f);
-    [Tooltip("Lower values means the destination position will be reached more quickly.")]
-    [SerializeField] private float takeOffSpeed = 3f;
+    [Tooltip("The amount of time the entire take off sequence will take.")]
+    [SerializeField] private float takeOffDuration = 3f;
     [Tooltip("The max amount of degrees allowed to turn on the Z axis.")]
     [SerializeField] private float maxRotationZ = 80f;
     [Tooltip("The highest y velocity magnitude considered when being used to lerp z rotation.")]
     [SerializeField] private float yVelocityTurnThreshold = 60f;
+    [SerializeField] private float rotationSpeedScaler = .125f;
     private Vector3 takeOffTargetPosition;
     private Vector3 velocity = Vector3.zero;
     private Vector3 rotationVelocity = Vector3.zero;
+    private float takeOffProgress = 0f;
+    private Vector3 takeOffStartingPosition;
+    private float takeOffStartTime;
     // Start is called before the first frame update
     IEnumerator Start()
     {
@@ -63,6 +67,8 @@ public class OwlController : MonoBehaviour
         this.takeOffTargetRotation = takeOffTargetRotation; 
         animState = takeOffType;
         owlState = OwlState.TakingOff;
+        takeOffStartingPosition = transform.position;
+        takeOffStartTime = Time.time;
     }
 
     public void InitiateTakeOff(Vector3 takeOffOffset, OwlAnimState takeOffType = OwlAnimState.TakeOffGrounded){
@@ -70,21 +76,24 @@ public class OwlController : MonoBehaviour
         takeOffTargetRotation = transform.eulerAngles; 
         animState = takeOffType;
         owlState = OwlState.TakingOff;
+        takeOffStartingPosition = transform.position;
+        takeOffStartTime = Time.time;
     }
 
     private void TakingOff(){
-        transform.position = Vector3.SmoothDamp(transform.position, takeOffTargetPosition, ref velocity, takeOffSpeed);
+        transform.position = Vector3.Lerp(takeOffStartingPosition, takeOffTargetPosition, takeOffProgress);
+        takeOffProgress = Mathf.Sin((Time.time - takeOffStartTime)/takeOffDuration * Mathf.PI / 2f);
+
         animator.speed = Mathf.Max(velocity.magnitude / 8f, .5f);
 
-        transform.localEulerAngles = Vector3.SmoothDamp(transform.localEulerAngles, takeOffTargetRotation, ref rotationVelocity, takeOffSpeed, 60f, Time.smoothDeltaTime);
-        print("rotationVelocity: " + rotationVelocity);
+        transform.localEulerAngles = Vector3.SmoothDamp(transform.localEulerAngles, takeOffTargetRotation, ref rotationVelocity, takeOffDuration * rotationSpeedScaler);
         if(rotationVelocity.y != 0f){
             float zRotation = Mathf.Lerp(0f, maxRotationZ, Mathf.Clamp(Mathf.Abs(rotationVelocity.y / yVelocityTurnThreshold), 0f, 1f));
             zRotation *= rotationVelocity.y < 0 ? 1 : -1;
             transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, zRotation);
         }
 
-        if(Vector3.Distance(transform.position, takeOffTargetPosition) < .2f){
+        if(Vector3.Distance(transform.position, takeOffTargetPosition) < .05f){
             transform.eulerAngles = takeOffTargetRotation;
             transform.position = takeOffTargetPosition;
             InitiateFlying();
